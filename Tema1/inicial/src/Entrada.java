55c42819953b8d0d9571edd6714b08269bbaeb97
Sikemig <sikemig@gmail.com>
2024-09-14 11:37:57 +0200

    primer proyecto

---
+++ Tema1/inicial/src/Entrada.java
@@ -0,0 +1,8 @@
+public class Entrada {
+    public static void main(String[]args){
+        System.out.println("Ejemplo de mensaje");
+    }
+
+    //Para añadir librerias que no esten en el jdk --> file --> project resources --> libraries y añadimos la ruta de la libreria descargada
+    // creamos un paquete a modo directorio para guardar las librerias (mysql connector) --> No es lo más recomendable, la libreria apunta a un directorio local
+}
