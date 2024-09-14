a02cc5be92b7ba040c788f27c83e602e1221bec0
Sikemig <sikemig@gmail.com>
2024-09-14 11:48:13 +0200

    comentarios en maven

--- Tema1/inicial-mv/src/main/java/Entrada.java
+++ Tema1/inicial-mv/src/main/java/Entrada.java
@@ -3,4 +3,10 @@ public class Entrada {
     public static void main(String[] args) {
         System.out.println("Este es un proyecto MAVEN");
     }
+    /*Estos proyectos nos permiten cargar librerias desde un servidor para que no estén en ningun equipo local
+    con lo que permite mejor cooperacion entre equipos
+    habría que buscar la libreria que necesitemos indicando que es proyecto MAVEN y meterlo en el pom.xml como una <dependencies>
+
+    Con los proyectos tipo Gradle pasaria lo mismo, pero cogeriamos la dependencia de tipo Gradle
+     */
 }
